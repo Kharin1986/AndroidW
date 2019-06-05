0164b778a453d34ea3ec320718a8b44165b66f45
Kharin1986 <harin_a_a@mail.ru>
2019-06-05 17:54:24 +0500

    HW5, Заглушка вместо графика, чекбокс передается

--- app/src/main/java/com/example/alex/hw/MainActivity.java
+++ app/src/main/java/com/example/alex/hw/MainActivity.java
@@ -1,35 +1,40 @@
 package com.example.alex.hw;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import static android.provider.Telephony.Mms.Part.TEXT;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     Spinner spinnerCity;
+    CheckBox showChartCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showChartCheckBox = (CheckBox) findViewById(R.id.checkBox);
         button = findViewById(R.id.buttonShow);
         button.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, ShowWeatherActivity.class));
+            Intent intent = new Intent(MainActivity.this, ShowWeatherActivity.class);
+            intent.putExtra(TEXT, showChartCheckBox.isChecked());  // Получить значение из чекбокса
+            startActivity(intent);
             finish();
         });
         spinnerCity = findViewById(R.id.spinner_city);
-        spinnerCity.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,new String[]{
+        spinnerCity.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{
                 getString(R.string.select_city),
                 getString(R.string.Moscow),
                 getString(R.string.St_Petersburg),
-                getString(R.string.Yekaterinburg),
-                ""
+                getString(R.string.Yekaterinburg)
         }));
     }
 
@@ -77,8 +82,8 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
     }
 
-    private void log (String string){
+    private void log(String string) {
         Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
-        Log.d("MA",string);
+        Log.d("MA", string);
     }
 }
